2b7e536f37fecaf4307af194ae36b367c4f4aa14
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2025-04-04 20:19:48 +0900

    Refactor import statement for ThemeSwitcher to align with updated directory structure, ensuring consistent access to components across the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import { ThemeProvider } from "next-themes";
 import { Geist } from "next/font/google";
 import Link from "next/link";
-import { ThemeSwitcher } from "../components/theme-switcher";
 import HeaderAuth from "./_components/header-auth";
 import "./globals.css";
 import { HeroUIProvider } from "@heroui/react";
