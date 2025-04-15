d1f99da02cb85e9710f32b7b79975bd1f43a4257
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2025-04-15 17:49:48 +0900

    Revert "Remove dashboard logic and replace with placeholder content"
    This reverts commit 0d871592a0a0fa85434b84e35eb628bf19663c1f.

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,3 +1,37 @@
+import Dashboard from "@/components/dashboard";
+import { getAllPredictions, getMonthlyPredictions } from "@/utils/predictions";
+import { getTotalBalance } from "@/utils/supabase/accounts";
+import { getUserOneTimeTransactions } from "@/utils/supabase/one-time-transactions";
+import { getUserRecurringTransactions } from "@/utils/supabase/recurring-transactions";
+import { updateAccountBalancesAction } from "./actions";
+
 export default async function DashboardPage() {
-	return <>hoge</>;
+	await updateAccountBalancesAction();
+
+	const oneMonthAgo = new Date();
+	oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+
+	const [
+		totalBalance,
+		predictions,
+		monthlyPredictions,
+		recurringTransactions,
+		recentTransactions,
+	] = await Promise.all([
+		getTotalBalance(),
+		getAllPredictions(),
+		getMonthlyPredictions(),
+		getUserRecurringTransactions(),
+		getUserOneTimeTransactions(undefined, oneMonthAgo, new Date()),
+	]);
+
+	return (
+		<Dashboard
+			totalBalance={totalBalance}
+			predictions={predictions}
+			monthlyPredictions={monthlyPredictions}
+			recurringTransactions={recurringTransactions}
+			recentTransactions={recentTransactions}
+		/>
+	);
 }
