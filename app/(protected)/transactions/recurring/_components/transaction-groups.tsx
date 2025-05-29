b06236a1d5e0ca55bda855d3132ab832e13c1631
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2025-05-29 09:40:29 +0900

    feat: auto-expand transaction groups by setting default accordion keys

--- app/(protected)/transactions/recurring/_components/transaction-groups.tsx
+++ app/(protected)/transactions/recurring/_components/transaction-groups.tsx
@@ -18,7 +18,7 @@ export const TransactionGroups = ({
 }: TransactionGroupsProps) => {
 	return (
 		<div className="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
-			<Accordion>
+			<Accordion defaultExpandedKeys={transactionGroups.map(group => group.accountId)}>
 				{transactionGroups.map((group) => (
 					<AccordionItem
 						key={group.accountId}
